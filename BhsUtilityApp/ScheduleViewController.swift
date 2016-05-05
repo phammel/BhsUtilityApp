7eee7d726c945d3826f211b79f0f5680a434183f
Spencer <student@shillestad.local>
2016-05-05 10:43:52 -0500

    finished classes

--- BhsUtilityApp/ScheduleViewController.swift
+++ BhsUtilityApp/ScheduleViewController.swift
@@ -12,7 +12,8 @@ class ScheduleViewController: UIViewController, UITableViewDataSource, UITableVi
 {
     @IBOutlet var myTableView: UITableView!
     
-    var defult = ["Clase 1","Class 2","Class 3","Class 2"]
+    @IBOutlet weak var editButton: UIBarButtonItem!
+   
     
      var classes = [Class]()
 
@@ -23,29 +24,93 @@ class ScheduleViewController: UIViewController, UITableViewDataSource, UITableVi
         super.viewDidLoad()
        myTableView.delegate = self
         myTableView.dataSource = self
+        editButton.tag = 0
     
-        classes.append(Class(teacher: "", roomNumber:"" ,hour: "", nameOfClass: ""))
-    }                           
+    }
     
     
    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return defult.count
+        return classes.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let myCell = myTableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
         
-        myCell.textLabel?.text = defult[indexPath.row]
-        
+        myCell.textLabel?.text = "\(classes[indexPath.row].teacher), \(classes[indexPath.row].roomNumber), \(classes[indexPath.row].hour), \(classes[indexPath.row].nameOfClass)"
         return myCell
     }
 
   
+    @IBAction func addClass(sender: AnyObject)
+    {
+        let myAlert = UIAlertController(title: "Add Class", message: nil, preferredStyle: .Alert)
+        myAlert.addTextFieldWithConfigurationHandler{ (nameTextField) -> Void in
+            nameTextField.placeholder = "Add Teachers!"
+        }
+        myAlert.addTextFieldWithConfigurationHandler{ (nameAilasField) -> Void in
+            nameAilasField.placeholder = "Add Room Number!"
+        }
+        
+        myAlert.addTextFieldWithConfigurationHandler{ (nameAilasField) -> Void in
+            nameAilasField.placeholder = "Add Class Hour"
+        }
+        myAlert.addTextFieldWithConfigurationHandler{ (webSiteTF) -> Void in
+            webSiteTF.placeholder = "Class Name"
+        }
+        
+        
+        let cancelAction = UIAlertAction(title: "Canel", style: UIAlertActionStyle.Cancel, handler: nil)
+        myAlert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (addAction) -> Void in
+            let teacherTF = myAlert.textFields![0] as UITextField
+            let roomNumberTF = myAlert.textFields![1] as UITextField
+            let  hourTF = myAlert.textFields![2] as UITextField
+            let  nameOfClassTF = myAlert.textFields![3] as UITextField
+            
+            self.classes.append(Class(teacher: teacherTF.text!, roomNumber: roomNumberTF.text! ,hour: hourTF.text!, nameOfClass: nameOfClassTF.text!))
+            self.myTableView.reloadData()
+
+    }
+        myAlert.addAction(addAction)
+        
+        self.presentViewController(myAlert, animated: true, completion: nil)
+
 
    
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete
+        {
+            classes.removeAtIndex(indexPath.row)
+            //reload tableview data
+            myTableView.reloadData()
+        }
+    }
+
+    
+    @IBAction func editButton(sender: AnyObject)
+    {
+        if editButton.tag == 0
+        {
+            myTableView.editing = true
+            editButton.tag = 1
+            
+        }
+        else
+        {
+            self.myTableView.editing = false
+            editButton.tag = 0
+        }
+
+    }
+    
+    
     
     
 
